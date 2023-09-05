c8f08a91e40a653aa3de75c754fccc10d62b539e
Sauerrr <128698531+Sauerrr@users.noreply.github.com>
2023-09-05 11:58:55 -0300

    Add files via upload

---
+++ Soma.js
@@ -0,0 +1,23 @@
+var num1=10;
+var num2=12;
+
+// console.log (num1+num2);
+
+var soma; 
+
+function calcularSoma(num1 , num2){
+  soma = num1 + num2;
+  return soma;
+}
+
+resultado = calcularSoma(num1, num2);
+console.log("A soma é:",resultado);
+
+num1 = 789
+num2 = 234
+
+resultado = calcularSoma(num1, num2);
+console.log("A soma é:", resultado);
+
+
+
